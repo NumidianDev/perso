da0456e2e805995935119d4d72f8bd0c290660fe
NumidianDev <amazighg@gmail.com>
2021-09-06 21:22:35 +0100

    a
    a

---
+++ setJavaHomeForAllUsersAtStartup.sh
@@ -0,0 +1,5 @@
+cat > /etc/profile.d/java8.sh <<EOF
+export JAVA_HOME=$(dirname $(dirname $(readlink $(readlink $(which javac)))))
+export PATH=$PATH:$JAVA_HOME/bin
+export CLASSPATH=.:$JAVA_HOME/jre/lib:$JAVA_HOME/lib:$JAVA_HOME/lib/tools.jar
+EOF
